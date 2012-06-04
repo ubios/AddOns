890d14c69c6b0be3d301a52d45e74553d4536ee7
Omega1970 <omega1970@gmail.com>
2012-06-04 12:42:58 +0200

    Added code to automatically change watched faction when reputation is gained.

--- ElvUI/modules/misc/exprepbar.lua
+++ ElvUI/modules/misc/exprepbar.lua
@@ -5,6 +5,11 @@ local BAR_WIDTH --Set post load so we can set it to a percent of your screen wid
 local BAR_HEIGHT = 9
 local TOPBAR_HEIGHT = ((BAR_HEIGHT + 2) * 4) + BAR_HEIGHT
 local showRepBar, showExpBar = false, false
+local find				= string.find
+local gsub				= string.gsub
+local incpat 			= gsub(gsub(FACTION_STANDING_INCREASED, "(%%s)", "(.+)"), "(%%d)", "(.+)")
+local changedpat	= gsub(gsub(FACTION_STANDING_CHANGED, "(%%s)", "(.+)"), "(%%d)", "(.+)")
+local decpat			= gsub(gsub(FACTION_STANDING_DECREASED, "(%%s)", "(.+)"), "(%%d)", "(.+)")
 
 local function GetXP(unit)
 	if(unit == 'pet') then
@@ -214,6 +219,32 @@ function M:UpdateRepBar(event)
 	M:PositionBars(self:GetNumShownBars())
 end
 
+function M:SetWatchedFactionOnReputationBar(event, msg)
+	local _, _, faction, amount = find(msg, incpat)
+	if not faction then _, _, faction, amount = find(msg, changedpat) or find(msg, decpat) end
+	if faction then
+		if faction == GUILD_REPUTATION then
+			faction = GetGuildInfo("player")
+		end
+
+		-- required for the GetNumFactions() function to return all factions in the game
+		ExpandAllFactionHeaders()
+	
+		local active = GetWatchedFactionInfo()
+		for factionIndex = 1, GetNumFactions() do
+			local name = GetFactionInfo(factionIndex)
+			if name == faction and name ~= active then
+				-- check if watch has been disabled by user
+				local inactive = IsFactionInactive(factionIndex)
+				if inactive == nil then
+					SetWatchedFactionIndex(factionIndex)
+				end
+				return
+			end
+		end
+	end
+end
+
 function M:UpdateExpRepBarAnchor()
 	UpperRepExpBarHolder:ClearAllPoints()
 	if E.db.general.expRepPos == 'TOP_SCREEN' then
@@ -292,5 +323,7 @@ function M:LoadExpRepBar()
 
 	--Reputation Events
 	self:RegisterEvent('UPDATE_FACTION', 'UpdateRepBar')
+	self:RegisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE", 'SetWatchedFactionOnReputationBar')
+	
 	OnLeave()
 end
