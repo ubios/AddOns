e4f097c5aa267455425b29b19798db95133193ca
Omega1970 <omega1970@gmail.com>
2012-09-02 19:15:20 +0200

    Added: Automatic changing of the watched faction on your reputation bar to the faction you got reputation points for.

--- ElvUI/modules/misc/exprepbar.lua
+++ ElvUI/modules/misc/exprepbar.lua
@@ -179,6 +179,32 @@ function M:CreateBar(name, onEnter, ...)
 	return bar
 end
 
+function M:SetWatchedFactionOnReputationBar(event, msg)
+	local _, _, faction, amount = find(msg, incpat)
+	if not faction then _, _, faction, amount = find(msg, changedpat) or find(msg, decpat) end
+	if faction then
+		if faction == GUILD_REPUTATION then
+			faction = GetGuildInfo("player")
+		end
+
+		-- required for the GetNumFactions() function to return all factions in the game
+		ExpandAllFactionHeaders()
+	
+		local active = GetWatchedFactionInfo()
+		for factionIndex = 1, GetNumFactions() do
+			local name = GetFactionInfo(factionIndex)
+			if name == faction and name ~= active then
+				-- check if watch has been disabled by user
+				local inactive = IsFactionInactive(factionIndex)
+				if inactive == nil then
+					SetWatchedFactionIndex(factionIndex)
+				end
+				return
+			end
+		end
+	end
+end
+
 function M:UpdateExpRepDimensions()
 	self.expBar:Width(E.db.general.experience.width)
 	self.expBar:Height(E.db.general.experience.height)
@@ -213,9 +239,11 @@ end
 function M:EnableDisable_ReputationBar()
 	if E.db.general.reputation.enable then
 		self:RegisterEvent('UPDATE_FACTION', 'UpdateReputation')
+		self:RegisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE", 'SetWatchedFactionOnReputationBar')
 		self:UpdateReputation()
 	else
 		self:UnregisterEvent('UPDATE_FACTION')
+		self:UnregisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE")
 		self.repBar:Hide()
 	end
 end
