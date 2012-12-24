5d23b647a798c84dce9d109ec6a4ac0754d60119
Omega1970 <omega1970@gmail.com>
2012-12-24 22:51:50 +0100

    Fixed CPU usage burst when showing skinned chat bubbles.

--- ElvUI/modules/misc/chatBubbles.lua
+++ ElvUI/modules/misc/chatBubbles.lua
@@ -4,8 +4,8 @@ local NP = E:GetModule("NamePlates");
 local numChildren = -1
 
 function M:UpdateBubbleBorder()
-	if not self:IsShown() or not self.text then return end
-	NP:SetVirtualBorder(self, self.text:GetTextColor())	
+	if not self.text then return end
+	NP:SetVirtualBorder(self, self.text:GetTextColor())
 end
 
 function M:SkinBubble(frame)
@@ -29,7 +29,7 @@ function M:SkinBubble(frame)
 	
 	frame:SetClampedToScreen(false)
 	frame.isBubblePowered = true
-	frame:HookScript('OnUpdate', M.UpdateBubbleBorder)
+	frame:HookScript('OnShow', M.UpdateBubbleBorder)
 end
 
 function M:IsChatBubble(frame)
@@ -43,14 +43,12 @@ function M:HookBubbles(...)
 	for index = 1, select('#', ...) do
 		local frame = select(index, ...)
 
-		if M:IsChatBubble(frame) and not frame.isBubblePowered then
-			M:SkinBubble(frame)
-		end
+		if M:IsChatBubble(frame) and not frame.isBubblePowered then	M:SkinBubble(frame) end
 	end
 end
 
 function M:LoadChatBubbles()
-	if not E.private.general.bubbles then return; end
+	if not E.private.general.bubbles then return end
 	CreateFrame('Frame'):SetScript('OnUpdate', function(self, elapsed)
 		if(WorldFrame:GetNumChildren() ~= numChildren) then
 			numChildren = WorldFrame:GetNumChildren()
