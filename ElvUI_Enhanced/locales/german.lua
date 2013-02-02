cc985400c6350b53faf30119a3db1a9ca6f66be2
Omega1970 <omega1970@gmail.com>
2013-02-02 18:49:16 +0100

    Added German translations.

--- ElvUI_Enhanced/locales/german.lua
+++ ElvUI_Enhanced/locales/german.lua
@@ -2,3 +2,51 @@
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI", "deDE")
 if not L then return end
+
+-- Init
+L["ENH_LOGIN_MSG"] = "Sie verwenden |cff1784d1ElvUI Enhanced|r Version %s%s|r."
+L["Your version of ElvUI is to old (required v5.1 or higher). Please, download the latest version from tukui.org."] = "Ihre Version von ElvUI ist zu alt (erforderlich v5.1 oder höher). Bitte laden Sie die neueste Version von tukui.org."
+
+-- Equipment
+L["Equipment"] = "Ausrüstung"
+L["EQUIPMENT_DESC"] = "Passen Sie die Einstellungen für das ändern Ihrer Ausrüstung an, wenn Sie Ihre Talentspezialisierung ändern oder ein Schlachtfeld betreten."
+L["No Change"] = "Keine Änderung"
+
+L["Specialization"] = "Talentspezialisierung"
+L["Enable/Disable the specialization switch."] = "Aktivieren / Deaktivieren des Talentspezialisierungsschalter."
+
+L["Primary Talent"] = "Primäre Talentspezialisierung"
+L["Choose the equipment set to use for your primary specialization."] = "Wählen Sie die Ausrüstungsvorlage fur Ihre Primäre Talentspezialisierung."
+
+L["Secondary Talent"] = "Sekundäre Talentspezialisierung"
+L["Choose the equipment set to use for your secondary specialization."] = "Wählen Sie die Ausrüstungsvorlage fur Ihre Sekundäre Talentspezialisierung."
+
+L["Battleground"] = "Schlachtfeld"
+L['Enable/Disable the battleground switch.'] = "Aktivieren / Deaktivieren des Sclachtfeldvorlagenschalter."
+
+L["Equipment Set"] = "Ausrüstungsvorlage"
+L["Choose the equipment set to use when you enter a battleground."] = "Wählen Sie die Ausrüstungsvorlage fur das Schlachtfeld."
+
+L["You have equipped equipment set: "] = "Sie haben die folgende Ausrüstungsvorlage ausgestattet: "
+
+-- GPS module
+L['Show the direction and distance to the selected party or raid member.'] = "Anzeigen von der Richtung und Entfernung zum ausgewählten Gruppen oder Schlachtzugsmitglied."
+
+-- Minimap Buttons
+L['Skin Buttons'] = "Skin Knöpfen"
+L['Skins the minimap buttons in Elv UI style.'] = "Skin die Minikarte Knöpfe in ElvUI Stil."
+L['Skin Style'] = "Skin Stil"
+L['Change settings for how the minimap buttons are skinned.'] = "Ändern der Einstellungen für wie die Minimap Knöpfen geskind werden."
+L['The size of the minimap buttons.'] = "Die Größe der Minikarte Knöpfen."
+
+L['No Anchor Bar'] = "Keine Ankerleiste"
+L['Horizontal Anchor Bar'] = "Horizontale Ankerleiste"
+L['Vertical Anchor Bar'] = "Vertikale Ankerleiste"
+
+-- PvP Autorelease
+L['PvP Autorelease'] = "PVP Automatische Freigabe"
+L['Automatically release body when killed inside a battleground.'] = "Automatisch freigeben des Körpers, wenn mann in einem Schlachtfeld getötet wird."
+
+-- Track Reputation
+L['Track Reputation'] = "Reputation Verfolgen"
+L['Automatically change your watched faction on the reputation bar to the faction you got reputation points for.'] = "Automatisch ändern der beobachteten Fraktion auf der Erfahrungsleiste zur Fraktion wo mann Reputationspunkte für bekommen hatt."
