c09856f155376befc1028772e7c350bc9bb986cf
Omega1970 <omega1970@gmail.com>
2013-02-04 22:30:48 +0100

    Added Portuguese language, translation by ubios.

--- ElvUI_Enhanced/locales/portuguese.lua
+++ ElvUI_Enhanced/locales/portuguese.lua
@@ -1,3 +1,64 @@
 ﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI", "ptBR")
 if not L then return end
+
+-- Translation by: ubios
+
+L["ENH_LOGIN_MSG"] = "Você está a usar |cff1784d1ElvUI Enhanced|r versão %s%s|r."
+L["Your version of ElvUI is to old (required v5.1 or higher). Please, download the latest version from tukui.org."] = "A sua versão do ElvUI é muita antiga (requerida v5.1 ou superior). Por favor, descarregue a versão mais recente em tukui.org."
+
+-- Equipment
+L["Equipment"] = "Equipamento"
+L["EQUIPMENT_DESC"] = "Ajuste os parâmetros para a mudança de equipamento quando muda de especialização ou entra num campo de batalha."
+L["No Change"] = "Sem mudança"
+
+L["Specialization"] = "Especialização"
+L["Enable/Disable the specialization switch."] = "Activar/desactivar a mudança com a especialização."
+
+L["Primary Talent"] = "Talento primário"
+L["Choose the equipment set to use for your primary specialization."] = "Escolha o conjunto de equipamento para usar com a especialização primária."
+
+L["Secondary Talent"] = "Talento secundário"
+L["Choose the equipment set to use for your secondary specialization."] = "Escolha o conjunto de equipamento para usar com a especialização secundária."
+
+L["Battleground"] = "Campo de batalha"
+L['Enable/Disable the battleground switch.'] = "Activar/desactivar a mudança em Campo de batalha."
+
+L["Equipment Set"] = "Conjunto de Equipamento"
+L["Choose the equipment set to use when you enter a battleground or arena."] = "Escolha o conjunto de equipamento para usar quando entra num Campo de batalha."
+
+L["You have equipped equipment set: "] = "Você tem equipado o conjunto: "
+
+L["DURABILITY_DESC"] = "Ajuste as opções para a informação de durabilidade no ecrã de informação do personagem."
+L['Enable/Disable the display of durability information on the character screen.'] = "Activar/desactivar a informação de durabilidade no ecrã de informação do personagem."
+
+L["Damaged Only"] = "Só Danificados"
+L["Only show durabitlity information for items that are damaged."] = "Só mostrar informação de durabilidade para itens danificados."
+
+-- GPS module
+L['GPS'] = "GPS"
+L['Show the direction and distance to the selected party or raid member.'] = "Mostrar a direcção e distância para o membro do grupo ou raide seleccionado."
+
+-- Minimap Buttons
+L['Skin Buttons'] = "Customizar Botões"
+L['Skins the minimap buttons in Elv UI style.'] = "Customiza os botões do minimapa no estilo do Elv UI."
+L['Skin Style'] = "Estilo de Customização"
+L['Change settings for how the minimap buttons are skinned.'] = "Mudar definições de como os botões do minimapa são customizados."
+L['The size of the minimap buttons.'] = "O tamanho dos botões do minimapa."
+
+L['No Anchor Bar'] = "Sem Ancora de Barra"
+L['Horizontal Anchor Bar'] = "Ancora de Barra Horizontal"
+L['Vertical Anchor Bar'] = "Ancora de Barra Vertical"
+
+-- PvP Autorelease
+L['PvP Autorelease'] = "Auto-libertar em JxJ"
+L['Automatically release body when killed inside a battleground.'] = "Automaticamente libertar o corpo quando morto num campo de batalha."
+
+-- Track Reputation
+L['Track Reputation'] = "Controlar Reputação"
+L['Automatically change your watched faction on the reputation bar to the faction you got reputation points for.'] = "Mudar automaticamente a facção controlada para a facção na qual acabou de ganhar pontos de reputação." 
+
+-- Extra Datatexts
+L['Actionbar1DataPanel'] = 'Actionbar 1'
+L['Actionbar3DataPanel'] = 'Actionbar 3'
+L['Actionbar5DataPanel'] = 'Actionbar 5'
