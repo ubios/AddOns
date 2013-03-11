86794f543be6d93955a573cedb9241c273a9b170
Omega1970 <omega1970@gmail.com>
2013-03-11 23:05:04 +0100

    Updated LibElvUIPlugin library to revision 10.

--- ElvUI_Enhanced/libs/LibElvUIPlugin-1.0/LibElvUIPlugin-1.0.lua
+++ ElvUI_Enhanced/libs/LibElvUIPlugin-1.0/LibElvUIPlugin-1.0.lua
@@ -1,7 +1,8 @@
-﻿if not ElvUI then return end
+if not ElvUI then return end
 
-local MAJOR, MINOR = "LibElvUIPlugin-1.0", 8
+local MAJOR, MINOR = "LibElvUIPlugin-1.0", 10
 local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
+
 
 if not lib then return end
 lib.plugins = {}
@@ -11,25 +12,10 @@ lib.index = 0
 --
 
 local E = ElvUI[1]
-local L = ElvUI[2]
 local _
-local concat, format, join = string.concat, string.format, string.join
-
---
--- Plugin table format:
---   { name (string) - The name of the plugin,
---     version (string) - The version of the plugin,
---     optionCallback (string) - The callback to call when ElvUI_Config is loaded
---   }
---
-
---
--- RegisterPlugin(name,callback)
---   Registers a module with the given name and option callback, pulls version info from metadata
---
 
 -- MULTI Language Support (Default Language: English)
-local MSG_OUTDATED = "Your version of %s is out of date. You can download the latest version from http://www.tukui.org"
+local MSG_OUTDATED = "Your version of %s is out of date (latest is version %d). You can download the latest version from http://www.tukui.org"
 local HDR_CONFIG = "Plugins"
 local HDR_INFORMATION = "LibElvUIPlugin-1.0.%d - Plugins Loaded  (Green means you have current version, Red means out of date)"
 local INFO_BY = "by"
@@ -45,6 +31,18 @@ if GetLocale() == "ruRU" then -- Russian Translations
 	INFO_NEW = "Последняя:"
 end
 
+--
+-- Plugin table format:
+--   { name (string) - The name of the plugin,
+--     version (string) - The version of the plugin,
+--     optionCallback (string) - The callback to call when ElvUI_Config is loaded
+--   }
+--
+
+--
+-- RegisterPlugin(name,callback)
+--   Registers a module with the given name and option callback, pulls version info from metadata
+--
 
 function lib:RegisterPlugin(name,callback)
 	local plugin = {}
@@ -53,7 +51,8 @@ function lib:RegisterPlugin(name,callback)
 	plugin.callback = callback
 	lib.plugins[name] = plugin
 	local enabled, loadable = select(4,GetAddOnInfo("ElvUI_Config"))
-	if enabled and loadable then
+	local loaded = IsAddOnLoaded("ElvUI_Config")
+	if enabled and loadable and not loaded then
 		if not lib.ConfigFrame then
 			local configFrame = CreateFrame("Frame")
 			configFrame:RegisterEvent("ADDON_LOADED")
@@ -98,67 +97,70 @@ function lib:SetupVersionCheck(plugin)
 		end
 	end
 	RegisterAddonMessagePrefix(prefix)
-	local function SendRecieve(self, event, mprefix, message, channel, sender)
-		if event == "CHAT_MSG_ADDON" then
-			if sender == E.myname or not sender or mprefix ~= prefix  or plugin.name == MAJOR then return end
-			
-			if not E[plugin.name.."recievedOutOfDateMessage"] then
-				if plugin.version ~= 'BETA' and tonumber(message) ~= nil and tonumber(message) > tonumber(plugin.version) then
-					plugin.old = true
-					plugin.newversion = tonumber(message)
-					local Pname = GetAddOnMetadata(plugin.name, "Title")
-					E:Print(format(MSG_OUTDATED, Pname))
-					E[plugin.name.."recievedOutOfDateMessage"] = true
+	local function SendRecieve(prefix)
+		return function(self, event, mprefix, message, channel, sender)
+			if event == "CHAT_MSG_ADDON" then
+				if sender == E.myname or not sender or mprefix ~= prefix  or plugin.name == MAJOR then return end
+				
+				if not E[plugin.name.."recievedOutOfDateMessage"] then
+					if plugin.version ~= 'BETA' and tonumber(message) ~= nil and tonumber(plugin.version) ~= nil and tonumber(message) > tonumber(plugin.version) then
+						plugin.old = true
+						plugin.newversion = tonumber(message)
+						local Pname = GetAddOnMetadata(plugin.name, "Title")
+						E:Print(format(MSG_OUTDATED,Pname,plugin.newversion))
+						E[plugin.name.."recievedOutOfDateMessage"] = true
+					end
 				end
+			else
+				E["Send"..plugin.name.."MSGTimer"] = E:ScheduleTimer("Send"..plugin.name.."VersionCheck", 12)
 			end
-		else
-			E["Send"..plugin.name.."MSGTimer"] = E:ScheduleTimer("Send"..plugin.name.."VersionCheck", 12)
 		end
 	end
 
 	local f = CreateFrame('Frame')
 	f:RegisterEvent("GROUP_ROSTER_UPDATE")
 	f:RegisterEvent("CHAT_MSG_ADDON")
-	f:SetScript('OnEvent', SendRecieve)
+	f:SetScript('OnEvent', SendRecieve(prefix))
 end
 
 function lib:GetPluginOptions()
 	E.Options.args.plugins = {
-		order = 10000,
-		type = "group",
-		name = HDR_CONFIG,
-		guiInline = false,
-		args = {
-			pluginheader = {
-				order = 1,
-				type = "header",
-				name = format(HDR_INFORMATION, MINOR),
-			},
-			plugins = {
-				order = 2,
-				type = "description",
-				name = lib:GeneratePluginList(),
-			},
-		}
-	}
+        order = 10000,
+        type = "group",
+        name = HDR_CONFIG,
+        guiInline = false,
+        args = {
+            pluginheader = {
+                order = 1,
+                type = "header",
+                name = format(HDR_INFORMATION, MINOR),
+            },
+            plugins = {
+                order = 2,
+                type = "description",
+                name = lib:GeneratePluginList(),
+            },
+        }
+    }
 end
+
 
 function lib:GeneratePluginList()
 	list = ""
 	for _, plugin in pairs(lib.plugins) do
 		if plugin.name ~= MAJOR then
 			local author = GetAddOnMetadata(plugin.name, "Author")
-			local Pname = GetAddOnMetadata(plugin.name, "Title")
+			local Pname = GetAddOnMetadata(plugin.name, "Title") or plugin.name
 			local color = plugin.old and E:RGBToHex(1,0,0) or E:RGBToHex(0,1,0)
-			list = concat(list, Pname)
+			list = list .. Pname 
 			if author then
-			  list = join(' ', list, INFO_BY, author)
+			  list = list .. " ".. INFO_BY .." " .. author
 			end
-			list = join('', list, color, " - ", INFO_VERSION, " ", plugin.version)
+			list = list .. color .. " - " .. INFO_VERSION .." " .. plugin.version
 			if plugin.old then
-			  list = join('', list, " (", INFO_NEW, " ", plugin.newversion, ")")
+			  list = list .. INFO_NEW .. plugin.newversion .. ")"
 			end
-			list = concat(list, "|r\n")
+			list = list .. "|r\n"
 		end
 	end
 	return list
