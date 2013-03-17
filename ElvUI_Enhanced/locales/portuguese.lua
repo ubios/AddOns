e047a52e6f5e69181a87d93da07c7e15e326cc44
Omega1970 <omega1970@gmail.com>
2013-03-17 01:16:53 +0100

    Changed minimum ElvUI version to 5.64 (Astrolabe library)

--- ElvUI_Enhanced/locales/portuguese.lua
+++ ElvUI_Enhanced/locales/portuguese.lua
@@ -5,7 +5,7 @@ if not L then return end
 -- Translation by: ubios
 
 L["ENH_LOGIN_MSG"] = "Você está a usar |cff1784d1ElvUI Enhanced|r versão %s%s|r."
-L["Your version of ElvUI is to old (required v5.61 or higher). Please, download the latest version from tukui.org."] = "A sua versão do ElvUI é muita antiga (requerida v5.61 ou superior). Por favor, descarregue a versão mais recente em tukui.org."
+L["Your version of ElvUI is to old (required v5.64 or higher). Please, download the latest version from tukui.org."] = "A sua versão do ElvUI é muita antiga (requerida v5.64 ou superior). Por favor, descarregue a versão mais recente em tukui.org."
 
 -- Equipment
 L["Equipment"] = "Equipamento"
