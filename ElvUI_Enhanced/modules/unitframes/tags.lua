b2e79e15048e20a9330fda825bf783e1899df57a
Omega1970 <omega1970@gmail.com>
2013-03-23 11:06:31 +0100

    Added: More efficient functions for [xnearbyplayers:x] tags.

--- ElvUI_Enhanced/modules/unitframes/tags.lua
+++ ElvUI_Enhanced/modules/unitframes/tags.lua
@@ -1,7 +1,9 @@
 local E, L, V, P, G = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB, Localize Underscore
 local UF = E:GetModule('UnitFrames')
 
-ElvUF.Tags.Methods['afk'] = function(unit)
+local twipe = table.wipe
+
+ElvUF.Tags.Methods['xafk'] = function(unit)
 	local isAFK, isDND = UnitIsAFK(unit), UnitIsDND(unit)
 	if isAFK then
 		return ('|cffFFFFFF[|r|cffFF0000%s|r|cFFFFFFFF]|r'):format(DEFAULT_AFK_MESSAGE)
@@ -10,14 +12,6 @@ ElvUF.Tags.Methods['afk'] = function(unit)
 	else
 		return ''
 	end
-end
-
-ElvUF.Tags.Methods['xdistance'] = function(unit)
-	local distance = E:GetDistance('player', unit)
-	if distance and distance > 0 then
-		return format('%d', distance)
-	end
-	return ''
 end
 
 ElvUF.Tags.Events['xthreat:percent'] = 'UNIT_THREAT_LIST_UPDATE GROUP_ROSTER_UPDATE'
@@ -52,4 +46,90 @@ ElvUF.Tags.Methods['xthreatcolor'] = function(unit)
 	else 
 		return '|cFF808080'
 	end
+end
+
+local GroupUnits = {}
+local f = CreateFrame("Frame")
+
+f:RegisterEvent("GROUP_ROSTER_UPDATE")
+f:SetScript("OnEvent", function()
+	local groupType, groupSize
+	twipe(GroupUnits)
+
+	if IsInRaid() then
+		groupType = "raid"
+		groupSize = GetNumGroupMembers()
+	elseif IsInGroup() then
+		groupType = "party"
+		groupSize = GetNumGroupMembers() - 1
+		GroupUnits["player"] = true
+	else
+		groupType = "solo"
+		groupSize = 1
+	end
+
+	for index = 1, groupSize do
+		local unit = groupType..index
+		if not UnitIsUnit(unit, "player") then
+			GroupUnits[unit] = true
+		end
+	end
+end)
+
+local timers = {
+}
+
+local function CheckElapsedTimeFunction(name, unit, elapsed, func, arg1)
+	timers[name] = timers[name] or {}
+	timers[name][unit] = timers[name][unit] or {}
+	if (GetTime() - (timers[name][unit][1] or 0)) > elapsed then
+		timers[name][unit][1] = GetTime()
+		timers[name][unit][2] = func(unit, arg1)
+	end
+	return timers[name][unit][2]
+end
+
+local function Distance(unit)
+	local distance = E:GetDistance('player', unit)	
+	if distance and distance > 0 then
+		return format('%d', distance)
+	end
+	return ''
+end
+
+local function NearbyPlayers(unit, range)
+	local unitsInRange, distance = 0
+	for groupUnit, _ in pairs(GroupUnits) do
+		if not UnitIsUnit(unit, groupUnit) and UnitIsConnected(groupUnit) then
+			distance = E:GetDistance(unit, groupUnit)
+			if distance and distance <= range then
+				unitsInRange = unitsInRange + 1
+			end
+		end
+	end
+	return unitsInRange
+end
+
+ElvUF.Tags.Methods['xdistance'] = function(unit)
+	if not UnitIsConnected(unit) or UnitIsUnit(unit, 'player') then return end
+
+	return CheckElapsedTimeFunction('distance', unit, .1, Distance)
+end
+
+ElvUF.Tags.Methods['xnearbyplayers:8'] = function(unit)
+	if not UnitIsConnected(unit) then return end
+
+	return CheckElapsedTimeFunction('nearbyplayers:8', unit, .25, NearbyPlayers, 8)
+end
+
+ElvUF.Tags.Methods['xnearbyplayers:10'] = function(unit)
+	if not UnitIsConnected(unit) then return end
+
+	return CheckElapsedTimeFunction('nearbyplayers:10', unit, .25, NearbyPlayers, 10)
+end
+
+ElvUF.Tags.Methods['xnearbyplayers:30'] = function(unit)
+	if not UnitIsConnected(unit) then return end
+
+	return CheckElapsedTimeFunction('nearbyplayers:30', unit, .25, NearbyPlayers, 30)
 end
