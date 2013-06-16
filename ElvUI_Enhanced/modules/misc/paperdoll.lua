66c9da5ab7213243cdf63faf7304cd4d480eb2a3
Tristimdorio <odinii@hotmail.com>
2013-06-16 10:39:55 +0200

    A better way to calculate heirloom item level, thnx Repooc.

--- ElvUI_Enhanced/modules/misc/paperdoll.lua
+++ ElvUI_Enhanced/modules/misc/paperdoll.lua
@@ -1,6 +1,8 @@
 local E, L, V, P, G = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB, Localize Underscore
 local PD = E:NewModule('PaperDoll', 'AceTimer-3.0', 'AceEvent-3.0');
 
+local find = string.find
+
 local slots = {
 	["HeadSlot"] = { true, true },
 	["NeckSlot"] = { true, false },
@@ -34,6 +36,12 @@ local levelColors = {
 	[2] = "|cffffff88",
 }
 
+-- From http://www.wowhead.com/items?filter=qu=7;sl=16:18:5:8:11:10:1:23:7:21:2:22:13:24:15:28:14:4:3:19:25:12:17:6:9;minle=1;maxle=1;cr=166;crs=3;crv=0
+local heirlooms = {
+	[80] = {44102,42944,44096,42943,42950,48677,42946,42948,42947,42992,50255,44103,44107,44095,44098,44097,44105,42951,48683,48685,42949,48687,42984,44100,44101,44092,48718,44091,42952,48689,44099,42991,42985,48691,44094,44093,42945,48716},
+}
+
+
 function PD:UpdatePaperDoll()
 	if InCombatLockdown() then
 		PD:RegisterEvent("PLAYER_REGEN_ENABLED", "UpdatePaperDoll")	
@@ -57,10 +65,10 @@ function PD:UpdatePaperDoll()
 				
         if itemLink then
             rarity, itemLevel = select(3, GetItemInfo(itemLink))    
-            linkLevel, upgrade = itemLink:match(":(%d+):%d+:(%d+)\124h%[")
-            if linkLevel and rarity == 7 then -- heirloom adjust
-                itemLevel = (tonumber(linkLevel) == UnitLevel("player")) and avgEquipItemLevel or linkLevel
+            if rarity == 7 then -- heirloom adjust
+                itemLevel = self:HeirLoomLevel(itemLink)
             end
+            upgrade = itemLink:match(":(%d+)\124h%[")
             if itemLevel and upgrade and levelAdjust[upgrade] then
                 itemLevel = itemLevel + levelAdjust[upgrade]
             end
@@ -82,6 +90,33 @@ function PD:UpdatePaperDoll()
 			end
 		end
 	end
+end
+
+function PD:HeirLoomLevel(itemLink)
+	local level = UnitLevel("player")
+	if level > 80 then
+		local _, _, _, _, itemId = find(itemLink, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?")
+		itemId = tonumber(itemId)
+		-- Downgrade it to 80 if found
+		for k, iid in pairs(heirlooms[80]) do
+			if iid == itemId then
+				level = 80
+				break
+			end
+		end	
+		-- There are currently no 1-90 Heirlooms
+		if level > 85 then level = 85 end
+	end
+	
+	if level > 80 then
+		return (( level - 80) * 26.6) + 200;
+	elseif level > 70 then
+		return (( level - 70) * 10) + 100;
+	elseif level > 60 then
+		return (( level - 60) * 4) + 60;
+	else
+		return level;
+	end	
 end
 
 function PD:InitialUpdatePaperDoll()
