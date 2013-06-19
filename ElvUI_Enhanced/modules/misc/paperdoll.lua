33424148b17f36ca2b810807561020b2da91ac01
Tristimdorio <odinii@hotmail.com>
2013-06-19 08:58:28 +0200

    Better method of calculating heirloom item levels. Updated level adjust table for item upgrades.

--- ElvUI_Enhanced/modules/misc/paperdoll.lua
+++ ElvUI_Enhanced/modules/misc/paperdoll.lua
@@ -27,18 +27,14 @@ local levelAdjust = {
 	["377"]=4,["379"]=4,["380"]=4,["445"]=0,["446"]=4,["447"]=8,
 	["451"]=0,["452"]=8,["453"]=0,["454"]=4,["455"]=8,["456"]=0,
 	["457"]=8,["458"]=0,["459"]=4,["460"]=8,["461"]=12,["462"]=16,
-	["465"]=0,["466"]=4,["467"]=8 
+	["465"]=0,["466"]=4,["467"]=8,["468"]=0,["469"]=4,["470"]=8,
+	["471"]=12,["472"]=16
 }
 
 local levelColors = {
 	[0] = "|cffff0000",
 	[1] = "|cff00ff00",
 	[2] = "|cffffff88",
-}
-
--- From http://www.wowhead.com/items?filter=qu=7;sl=16:18:5:8:11:10:1:23:7:21:2:22:13:24:15:28:14:4:3:19:25:12:17:6:9;minle=1;maxle=1;cr=166;crs=3;crv=0
-local heirlooms = {
-	[80] = {44102,42944,44096,42943,42950,48677,42946,42948,42947,42992,50255,44103,44107,44095,44098,44097,44105,42951,48683,48685,42949,48687,42984,44100,44101,44092,48718,44091,42952,48689,44099,42991,42985,48691,44094,44093,42945,48716},
 }
 
 function PD:UpdatePaperDoll()
@@ -96,31 +92,27 @@ function PD:GetItemLevel(itemLink)
 	return itemLevel
 end
 
+--[[heirloom ilvl equivalents
+Vanilla: 1-60 = 60 / 60 = scales by 1 ilvl per player level
+TBC rares: 85-115 = 30 / 10 = scales by 3 ilvl per player level
+WLK rares: 130-190(200) = 60 / 5 = scales by 12 ilvl per player level
+CAT rares: 272-333 = 61/5 = scales by 12.2 ilvl per player level
+MOP rares: 364-450 = 86/5 = scales by 17.2 ilvl per player level (guesswork)
+]]
 function PD:HeirLoomLevel(itemLink)
-	local level = UnitLevel("player")
-	if level > 80 then
-		local _, _, _, _, itemId = find(itemLink, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?")
-		itemId = tonumber(itemId)
-		-- Downgrade it to 80 if found
-		for k, iid in pairs(heirlooms[80]) do
-			if iid == itemId then
-				level = 80
-				break
-			end
-		end	
-		-- There are currently no 1-90 Heirlooms
-		if level > 85 then level = 85 end
-	end
-	
-	if level > 80 then
-		return (( level - 80) * 26.6) + 200;
-	elseif level > 70 then
-		return (( level - 70) * 10) + 100;
-	elseif level > 60 then
-		return (( level - 60) * 4) + 60;
-	else
-		return level;
-	end	
+    local level = UnitLevel("player")
+    local linkLevel = itemLink:match(":(%d+):%d+:%d+\124h%[")
+    level = min(tonumber(linkLevel),level)
+
+    if level > 80 then -- CAT heirloom scaling kicks in at 81
+        return (( level - 81) * 12.2) + 272;
+    elseif level > 67 then -- WLK heirloom scaling kicks in at 68
+        return (( level - 68) * 12) + 130;
+    elseif level > 59 then -- TBC heirloom scaling kicks in at 60
+        return (( level - 60) * 3) + 85;
+    else
+        return level;
+    end 
 end
 
 function PD:InitialUpdatePaperDoll()
